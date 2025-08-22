bca00399c820fa57d5b0897877786d8be660a893
AMIT SHARMA <amit8102006sharma@gmail.com>
2025-08-22 22:37:14 +0530

    Add files via upload

---
+++ AMIT ASSIGNMENTS .py
@@ -0,0 +1,26 @@
+# ASSIGNMENT NO 1 OF TUTEDUDE BY AMIT SHARMA
+#QUESTION NO. 1 
+num1=  int(input('ENTER THE NUMBER 1: '))
+num2= int (input('ENTER THE NUMBER 2: '))
+num3= (num1+num2)
+print('addition:',num3)
+
+# substraction
+num3 = (num1-num2)
+print('SUBSTRACTION:',num3)
+
+# multiplication
+num3 = (num1*num2)
+print('MULTIPLICATION:',num3)
+
+#division
+num3 = (num1/num2)
+print('DIVISION:',num3)
+# QUESTION NO 2
+#THIS IS FIRST ASSIGNMENT OF AMIT SHARMA TO RINT GREETNGS TO THE GIVEN INPUT
+
+first = input('ENTER YOUR FIRST NAME ')
+
+last = input('ENTER YOUR LAST NAME ')
+
+print('Hey ! ', first ,last,' Nice to meet you \n \tTHIS IS AMIT SHARMA  \n\t2ND TASK ON TUTEDUDE ' )
