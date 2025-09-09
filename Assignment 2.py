a61022895f31de2a80d58029eec71fa0f0d5bd98
AMIT SHARMA <amit8102006sharma@gmail.com>
2025-09-09 15:10:19 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,8 @@
+#assignment no. 2 tutedude
+number = int (input ("Enter a number: "))
+# taking input from user
+if number % 2 == 0:
+    print(f"{number} is even")
+    #checking the no\umber is even or not
+else:
+    print(f"{number} is odd")
